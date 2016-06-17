59ddde1d2ccdc2a5420ea6ec82828fa26167475a
Shon <nerftactic09@gmail.com>
2016-06-17 12:46:17 +0000

    Added user profile pages. Added username sign-up form.

--- db/migrate/20160616123838_add_username_to_users.rb
+++ db/migrate/20160616123838_add_username_to_users.rb
@@ -2,6 +2,5 @@ class AddUsernameToUsers < ActiveRecord::Migration
   def change # anything inside of this method is translated to SQL code.
     add_column :users, :username, :string # add a new column to tablet "users", called "username" and of type "string".
     add_index :users, :username, unique: true # first, index usernames for quick (rapid) lookup.
-    
   end
 end
